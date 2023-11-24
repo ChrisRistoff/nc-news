48b8fe6dfec85cf199c040a2ab933c3de04b9b25
Krasyo <doanusho@gmail.com>
2023-11-24 22:39:16 +0000

    CD add deploy yaml file in workflows and add deploy script

---
+++ scripts/deploy_script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+CONTAINER_NAME="nc_news_c"
+IMAGE_NAME="nc_news"
+APP_DIR="/nc_news"
+
+echo "Starting deployment..."
+
+cd "$APP_DIR"
+
+echo "Pulling latest changes from Git..."
+git pull origin main
+
+echo "Stopping the existing Docker container..."
+sudo docker stop $CONTAINER_NAME || true
+sudo docker rm $CONTAINER_NAME || true
+
+echo "Building the new Docker image..."
+sudo docker build -t $IMAGE_NAME .
+
+echo "Running the new Docker container..."
+sudo docker run -p 80:80 --name $CONTAINER_NAME $IMAGE_NAME
+
+echo "Removing unused Docker images..."
+sudo docker image prune -af
+
+echo "Deployment successful!"
