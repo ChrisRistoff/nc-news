7115012e889b5268a90aa195f6c693bbac250f7b
Krasyo <doanusho@gmail.com>
2023-11-24 23:08:28 +0000

    changed port for daemon... fixed now

--- scripts/deploy_script.sh
+++ scripts/deploy_script.sh
@@ -21,7 +21,7 @@ echo "Building the new Docker image..."
 sudo docker build -t $IMAGE_NAME .
 
 echo "Running the new Docker container..."
-sudo docker run -d 80:80 --name $CONTAINER_NAME $IMAGE_NAME
+sudo docker run -p 80:80 -d --name $CONTAINER_NAME $IMAGE_NAME
 
 echo "Removing unused Docker images..."
 sudo docker image prune -af
