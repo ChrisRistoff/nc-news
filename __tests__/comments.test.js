59e98eb54c387757d9099ddf230a7d059efb9f11
Krasyo <doanusho@gmail.com>
2023-11-24 12:22:56 +0000

    Edit comment Add edit comment router, controller, middleware and tests. Change endpoints.json to include edit comment and change some of the rest of the routes because they were not updated properly.

--- __tests__/comments.test.js
+++ __tests__/comments.test.js
@@ -1,4 +1,3 @@
-require("jest-sorted");
 const supertest = require("supertest");
 const data = require("../db/data/test-data/index");
 const seed = require("../db/seeds/seed");
@@ -129,153 +128,6 @@ describe("get comments for article", () => {
   });
 });
 
-describe("create comments", () => {
-  it("POST 201: Should create a new comment for an article", async () => {
-    const res = await supertest(app)
-      .post("/api/articles/1/comments")
-      .send({
-        body: "test body",
-      })
-      .set("Authorization", `Bearer ${token}`);
-
-    const comment = res.body.comment;
-
-    expect(res.statusCode).toBe(201);
-    expect(comment.author).toBe("test");
-    expect(comment.body).toBe("test body");
-    expect(comment.article_id).toBe(1);
-    expect(comment).toHaveProperty("comment_id");
-  });
-
-  it("POST 201: Should create a new comment for an article when extra parameters in the body are given", async () => {
-    const res = await supertest(app)
-      .post("/api/articles/1/comments")
-      .send({
-        body: "test body",
-        test: "testing",
-      })
-      .set("Authorization", `Bearer ${token}`);
-
-    const comment = res.body.comment;
-
-    expect(res.statusCode).toBe(201);
-    expect(comment.author).toBe("test");
-    expect(comment.body).toBe("test body");
-    expect(comment.article_id).toBe(1);
-    expect(comment).toHaveProperty("comment_id");
-  });
-
-  it("POST 401: Should return an error when the user is not logged in", async () => {
-    const res = await supertest(app).post("/api/articles/1/comments").send({
-      body: "test body",
-    });
-
-    expect(res.statusCode).toBe(401);
-    expect(res.body.msg).toBe("You need to be logged in");
-  });
-
-  it("POST 400: Should return an error when user gives invalid input for article ID", async () => {
-    const res = await supertest(app)
-      .post("/api/articles/asdsa/comments")
-      .send({
-        username: "butter_bridge",
-        body: "test body",
-      })
-      .set("Authorization", `Bearer ${token}`);
-
-    expect(res.statusCode).toBe(400);
-    expect(res.body.msg).toBe("Invalid input");
-  });
-
-  it("POST 404: Should return an error article with ID does not exist", async () => {
-    const res = await supertest(app)
-      .post("/api/articles/12312312/comments")
-      .send({
-        username: "butter_bridge",
-        body: "test body",
-      })
-      .set("Authorization", `Bearer ${token}`);
-
-    expect(res.statusCode).toBe(404);
-    expect(res.body.msg).toBe("Article ID not found");
-  });
-
-  it("POST 400: Should return an error when body is empty", async () => {
-    const res = await supertest(app)
-      .post("/api/articles/1/comments")
-      .send({
-        username: "butter_bridge",
-        body: "",
-      })
-      .set("Authorization", `Bearer ${token}`);
-
-    expect(res.statusCode).toBe(400);
-    expect(res.body.msg).toBe("Body can not be empty");
-  });
-
-  it("POST 400: Should return an error when body is missing", async () => {
-    const res = await supertest(app)
-      .post("/api/articles/1/comments")
-      .send({
-        username: "butter_bridge",
-      })
-      .set("Authorization", `Bearer ${token}`);
-
-    expect(res.statusCode).toBe(400);
-    expect(res.body.msg).toBe("Body can not be empty");
-  });
-});
-
-describe("delete comment", () => {
-  it("DELETE 204: Should delete comment", async () => {
-    const res = await supertest(app)
-      .delete("/api/comments/1")
-      .set("Authorization", `Bearer ${token}`);
-
-    expect(res.statusCode).toBe(204);
-  });
-
-  it("POST 200, 204: Comment should be deleted", async () => {
-    const comments = await supertest(app).get("/api/articles/1/comments");
-    expect(comments.statusCode).toBe(200);
-
-    const commentId = comments.body.comments[0].comment_id;
-
-    const deleteComment = await supertest(app)
-      .delete(`/api/comments/${commentId}`)
-      .set("Authorization", `Bearer ${token}`);
-
-    expect(deleteComment.statusCode).toBe(204);
-
-    const updatedComments = await supertest(app).get(
-      "/api/articles/1/comments",
-    );
-    expect(updatedComments.statusCode).toBe(200);
-
-    for (const comment of updatedComments.body.comments) {
-      expect(comment.comment_id !== commentId).toBe(true);
-    }
-  });
-
-  it("DELETE 400: Should return an error when invalid commend ID is given", async () => {
-    const res = await supertest(app)
-      .delete("/api/comments/asds")
-      .set("Authorization", `Bearer ${token}`);
-
-    expect(res.statusCode).toBe(400);
-    expect(res.body.msg).toBe("Invalid input");
-  });
-
-  it("DELETE 404: Should return an error when article ID does not exist", async () => {
-    const res = await supertest(app)
-      .delete("/api/comments/1200")
-      .set("Authorization", `Bearer ${token}`);
-
-    expect(res.statusCode).toBe(404);
-    expect(res.body.msg).toBe("Comment does not exist");
-  });
-});
-
 describe("update comment by ID", () => {
   it("PATCH 200: Should return an updated comment to the user", async () => {
     const res = await supertest(app)
@@ -286,9 +138,9 @@ describe("update comment by ID", () => {
     expect(res.body.newComment).toEqual(
       expect.objectContaining({
         body: expect.any(String),
-        votes: 114,
-        author: "butter_bridge",
-        article_id: 1,
+        votes: 116,
+        author: "test",
+        article_id: 9,
         created_at: expect.any(String),
       }),
     );
