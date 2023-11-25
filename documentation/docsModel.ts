ae83a5bff549192c63096690166226d3d04f5de2
Krasyo <doanusho@gmail.com>
2023-11-25 22:09:32 +0000

    everything seems to work besides the documentation page

--- documentation/docsModel.ts
+++ documentation/docsModel.ts
@@ -3,6 +3,5 @@ import fs from "fs/promises"
 export const getDocsModel = async () => {
   const docs = await fs.readFile(`${__dirname}/../endpoints.json`, "utf-8")
 
-  // @ts-ignore
-  return JSON.parse(docs, null, 2)
+  return JSON.parse(docs)
 }
