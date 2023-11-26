b4fc8b5c2d84d0bcf35fbfd6bbaaae7f23c35e53
Krasyo <doanusho@gmail.com>
2023-11-26 10:54:06 +0000

    Docs Docs path gave me a server error when running in AWS, made small changes to the path, this should fix it

--- documentation/docsModel.ts
+++ documentation/docsModel.ts
@@ -1,7 +1,16 @@
 import fs from "fs/promises"
 
 export const getDocsModel = async () => {
-  const docs = await fs.readFile(`${__dirname}/../endpoints.json`, "utf-8")
+  const ENV = process.env.NODE_ENV
+  let path: string
+
+  if(ENV === "test") {
+    path = `${__dirname}/../endpoints.json`
+  } else {
+    path = `${__dirname}/../../endpoints.json`
+  }
+
+  const docs = await fs.readFile(path, "utf-8")
 
   return JSON.parse(docs)
 }
