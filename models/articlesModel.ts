887a738d568f8e21b25f4c3d9c0ed7c060ae2794
Krasyo <doanusho@gmail.com>
2023-12-16 23:42:46 +0000

    pushing for deployment

--- models/articlesModel.ts
+++ models/articlesModel.ts
@@ -106,15 +106,6 @@ export const getAllArticlesModel = async (
   if (topic) totalCountParams.push(topic);
 
   total_count = await db.query(total_countQuery, totalCountParams)
-/*
-  if (topic) {
-    total_countQuery += !search ? `WHERE topic = $1 AND (title ILIKE $2 OR body ILIKE $2 OR topic ILIKE $2 OR author ILIKE $2)`;
-    total_count = await db.query(total_countQuery, params);
-  }
-  else {
-    total_countQuery += `WHERE (title ILIKE $1 OR body ILIKE $1 OR topic ILIKE $1 OR author ILIKE $1)`;
-    total_count = await db.query(total_countQuery, params);
- }*/
 
   total_count = total_count.rows[0].total_count;
 
