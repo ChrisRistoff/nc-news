8942c4513ddcd2a628b27d882094f476782526da
Krasyo <doanusho@gmail.com>
2024-02-04 14:55:21 +0000

    Readability Add vertical spacing and comments to middleware for easier navigation

--- middleware/paginate.ts
+++ middleware/paginate.ts
@@ -1,11 +1,18 @@
 export const paginateQuery = (dbQuery: string, page: number, limit: number) => {
+
+  // if page or limit are not passed, set default values
   page = page ? Number(page) : 1;
   limit = limit ? Number(limit) : 10;
 
+  // if page or limit are not numbers, return false
   if (isNaN(page) || isNaN(limit)) return false;
 
+  // calculate the offset
   const offset = (page - 1) * limit;
+
+  // add the limit and offset to the query
   dbQuery += ` LIMIT ${limit} OFFSET ${offset}`;
 
+  // return the query
   return dbQuery;
 };
