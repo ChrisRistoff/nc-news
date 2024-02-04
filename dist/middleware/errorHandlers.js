3045bf25c1bc5a0bfe06fbdf19a9d47eebe922f2
Krasyo <doanusho@gmail.com>
2024-02-04 14:55:57 +0000

    Readability Add vertical spacing and comments to middleware for easier navigation

--- dist/middleware/errorHandlers.js
+++ dist/middleware/errorHandlers.js
@@ -2,26 +2,33 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serverError = exports.customErrors = exports.sqlErrors = void 0;
 const sqlErrors = (err, _, res, next) => {
+    // invalid input
     if (err.code === "22P02") {
         return res.status(400).send({ msg: "Invalid input" });
     }
+    // bad request
     if (err.code === "23503") {
         return res.status(400).send({ msg: "Bad request" });
     }
+    // already exists
     if (err.code === "23505") {
         return res.status(409).send({ msg: "Already exists" });
     }
+    // go to the next error handler
     next(err);
 };
 exports.sqlErrors = sqlErrors;
 const customErrors = (err, _, res, next) => {
+    // if the error has an error code, return the error code and message
     if (err.errCode) {
         return res.status(err.errCode).send({ msg: err.errMsg });
     }
+    // go to the next error handler
     next(err);
 };
 exports.customErrors = customErrors;
 const serverError = (err, _, res, __) => {
+    // for any other error, return 500 and log the error
     console.log(err);
     return res.status(500).send({ msg: "Internal server error" });
 };
--- dist/middleware/paginate.js
+++ dist/middleware/paginate.js
@@ -2,12 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.paginateQuery = void 0;
 const paginateQuery = (dbQuery, page, limit) => {
+    // if page or limit are not passed, set default values
     page = page ? Number(page) : 1;
     limit = limit ? Number(limit) : 10;
+    // if page or limit are not numbers, return false
     if (isNaN(page) || isNaN(limit))
         return false;
+    // calculate the offset
     const offset = (page - 1) * limit;
+    // add the limit and offset to the query
     dbQuery += ` LIMIT ${limit} OFFSET ${offset}`;
+    // return the query
     return dbQuery;
 };
 exports.paginateQuery = paginateQuery;
